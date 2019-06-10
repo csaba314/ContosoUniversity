2bf3b805e941dfe6960adf9fd0bbc7a300508cdd
Csaba Erdes <csaba.erdes@gmail.com>
2019-06-10 17:04:12 +0200

    testing connection resiliency and command interception

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -6,6 +6,10 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
+
 namespace ContosoUniversity
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -16,6 +20,9 @@ namespace ContosoUniversity
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //DbInterception.Add(new SchoolInterceptorTransientErrors());
+            //DbInterception.Add(new SchoolInterceptorLogging());
         }
     }
 }
